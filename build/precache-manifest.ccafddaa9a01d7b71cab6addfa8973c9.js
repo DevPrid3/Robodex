fffd2f75e3693b23d51cc0c94d97614cf5784613
DevPrid3 <prid3.costa@gmail.com>
2020-04-29 22:09:51 +0200

    adding files for github pages

---
+++ build/precache-manifest.ccafddaa9a01d7b71cab6addfa8973c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923cb57cf04b76787a553c7ced5f91a2",
+    "url": "/robodex/index.html"
+  },
+  {
+    "revision": "01782dddb2dcbc1fc7b4",
+    "url": "/robodex/static/css/main.88c133e6.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/robodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01782dddb2dcbc1fc7b4",
+    "url": "/robodex/static/js/main.4e0ff870.chunk.js"
+  },
+  {
+    "revision": "059a62aed603723008b2",
+    "url": "/robodex/static/js/runtime-main.228ebcc0.js"
+  }
+]);
